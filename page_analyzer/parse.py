86acca6da1bf2b8df7f1542190f2272dd7ed8a3a
a.polevodin <a.polevodin@vkteam.ru>
2026-04-08 00:04:40 +0300

    page analyzer

--- page_analyzer/parse.py
+++ page_analyzer/parse.py
@@ -5,9 +5,13 @@ def parse_response(response):
     content = BeautifulSoup(response.content, "html.parser")
     meta = content.find('meta', {'name': 'description'})
 
+    h1 = content.h1.text if content.h1 else None
+    title = content.title.string if content.title else None
+    description = meta.get('content') if meta else None
+
     return {
         'status': response.status_code,
-        'h1': content.h1.text if content.h1 else None,
-        'title': content.title.string if content.title else None,
-        'description': meta.get('content') if meta else None,
+        'h1': f'{h1[:200]}...' if len(h1) > 200 else h1,
+        'title': f'{title[:200]}...' if len(title) > 200 else title,
+        'description': f'{description[:200]}...' if len(description) > 200 else description,
     }
