0c9c80a4d9042caa85da9d8cff22efb611f856aa
a.polevodin <a.polevodin@vkteam.ru>
2026-04-08 00:16:34 +0300

    page analyzer

--- page_analyzer/validate.py
+++ page_analyzer/validate.py
@@ -4,8 +4,8 @@ import validators
 def validate_url(url):
     errors = []
     if not validators.url(url):
-        errors.append('invalid URL.')
+        errors.append('Некорректный URL')
     if len(url) > 255:
-        errors.append('URL must be no longer than 255 characters.')
+        errors.append('URL должен быть короче 255 символов')
     print(errors)
     return errors
